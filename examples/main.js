4b303dfc152424508b97fcff41b37aec8dbb4ee9
ACodeSinger <15521111612@163.com>
2019-03-15 18:14:23 +0800

    update local remote

--- examples/main.js
+++ examples/main.js
@@ -5,7 +5,7 @@ import MessageBox from '../packages/index';
 
 Vue.prototype.$alert = MessageBox.alert;
 Vue.prototype.$alert.config = {
-  // confirmButtonText: '好的',
+  // confirmButtonText: '好的！',
   // confirmButtonTextColor: 'green',
 };
 Vue.prototype.$confirm = MessageBox.confirm;
